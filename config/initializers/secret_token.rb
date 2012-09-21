c97048a8e14557b9f4ab03daba9fa705c4d834cd
Luis Urraca <l.urraca@gmail.com>
2012-09-21 19:48:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsScrapper::Application.config.secret_token = '0a5f90eb4087453eb725e634baf63a8f2daefbf0e6bfafc0aedee12dcc01613e73d066ed7f5c2c75a3a0f86a8f734a633e4d45f3d7d6cc37f9045ef838b392ba'
